f3f5b65d3947f5d2ebd6dae5a7d8e39803481495
Jason Madrid <jasonhnn6@gmail.com>
2026-05-16 19:54:59 -0600

    Agregando get y post

--- carrito-tg/backend/src/server.ts
+++ carrito-tg/backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import Carrito from "./models/Carrito";
 import "./config/db";
 
 dotenv.config();
@@ -9,6 +10,32 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/carrito", async (req, res) => {
+  try {
+    const carrito = await Carrito.findAll();
+    if (carrito.length > 0) {
+      res.status(200).json(carrito);
+    } else {
+      res.status(404).json({ error: "Carrito no encontrado" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener el carrito" });
+  }
+});
+
+app.post("/api/carrito", async (req, res) => {
+  try {
+    const producto = await Carrito.create(req.body);
+    if (producto !== null) {
+      res.status(201).json(producto);
+    } else {
+      res.status(400).json({ error: "Error al agregar al carrito" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: "Error al agregar al carrito" });
+  }
+});
 
 const PORT = process.env.PORT || 3001;
 
